28d6ad059ae369a8bab12194714e377ab3d181a3
ybeshkov <91119020+ybeshkov@users.noreply.github.com>
2023-01-24 20:08:39 +0300

    Add files via upload

---
+++ lab odnom massiv.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+int main(int argc, const char* argv[]) {
+    setlocale(LC_ALL, "Russian");
+    int array[] = { 1, 3, 5, 7, 9, 11 };
+    int d = array[1] - array[0];
+    int length = sizeof(array) / sizeof(*array);
+    for (unsigned i = 1; i < length - 1; i++) {
+        if (array[i + 1] - array[i] != d) {
+            std::cout << "нет арифметической прогрессии" << std::endl;
+            return 0;
+        }
+    }
+    std::cout << "есть арифметическая прогрессия. ее разность равна " << d << std::endl;
+    return 0;
+}
